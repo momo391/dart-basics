25813099f2372819887e87188f0fc69b7e9e1257
momo391 <ia7636hs@gmail.com>
2024-10-26 23:23:23 +0100

    dart basics

---
+++ bin/net_ninja_course.dart
@@ -0,0 +1,113 @@
+void main() {
+  String name = "Abhishek";
+  print(name);
+}
+
+//! 1-dart basics:
+// dart is a static typed language
+// void main () {} : for running main function.
+// var <var name> = <var value> : for creating variable , var will automatically get its type from declaration.
+// semicolon is required at the end of the line.
+// final instead of var: for creating final variable and value will be initialized at runtime ( don't know the value in compile time ).
+// const instead of var: for creating const variable and value will be initialized at compile time ( know the value in compile time ).
+// operations is the same as java.
+// print() : function for printing variable.
+// $<var name>: for string interpolation using variables.
+// ${}: for string interpolation using expressions.
+
+//! 2-dart data types:
+// int : for integer
+// double : for floating point number
+// String : for string
+// bool : for boolean
+// dynamic : for any data type
+// List : for list
+
+//! 3-dart operators:
+// + : for addition
+// - : for subtraction
+// * : for multiplication
+// / : for division
+// % : for modulus
+// ~/ : for integer division
+// ++ : for increment
+// -- : for decrement
+// == : for equality
+// != : for inequality
+// < : for less than
+// > : for greater than
+// <= : for less than or equal to
+// >= : for greater than or equal to
+// && : for logical and
+// || : for logical or
+// ! : for logical not
+// ? : for ternary operator
+
+//! 4-dart functions:
+// () : for function
+// {} : for function body
+// return : for returning value
+// () => : for lambda function
+
+//! 5-dart classes:
+// class : for creating class
+// extends : for extending class
+// implements : for implementing interface
+// with : for using mixin
+// {} : for class body
+
+//! 6-dart loops:
+// for : for loop
+// while : while loop
+// do : do while loop
+// for in : for in loop
+// break : for breaking loop
+// continue : for continuing loop
+
+//! 7-dart null safety:
+// ? : for nullable
+// ! : for non-nullable
+// ?? : for null-aware operator
+// ??= : for null-aware assignment operator
+// late : for late initialization 
+
+//! 8-dart packages:
+// pubspec.yaml : for pubspec file
+// pub : for pub command
+// get : for getting package
+// run : for running package
+// dev : for developing package
+// web : for running web application  
+
+//! 9-dart collections:
+// List : for list
+// Set : for set
+// Map : for map
+
+//! 10-dart exceptions:
+// try : for handling exception
+// throw : for throwing exception
+// catch : for handling exception
+
+//! 11-dart concurrency:
+// async : for asynchronous
+// await : for waiting for asynchronous task
+// Future : for future
+// Stream : for stream
+
+//! 12-dart testing:
+// test : for testing
+// expect : for expecting value
+// main : for main function
+// dart test : for running test
+// flutter test : for running test
+// flutter run : for running application
+// flutter build : for building application
+// flutter pub get : for getting package
+// flutter pub run : for running package
+// flutter pub upgrade : for upgrading package
+// flutter pub publish : for publishing package
+// flutter pub outdated : for checking outdated package
+// flutter pub upgrade <package name> : for upgrading package
+// flutter pub run <package name> : for running package
+// flutter pub run <package name> <arguments> : for running package with arguments
---
+++ test/net_ninja_course_test.dart
@@ -0,0 +1,8 @@
+import 'package:net_ninja_course/net_ninja_course.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
